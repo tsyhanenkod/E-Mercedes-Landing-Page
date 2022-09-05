49c7404683a90da880b6f4246920a497b9ac162f
Den T <tsyhanenko.dp@gmail.com>
2022-09-05 14:15:01 +0300

    ready slider

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ $(function(){
   $('.ds_slider').slick({
     dots: false,
     slidesToShow: 4,
-    variableWidth: true
+    variableWidth: true,
+    prevArrow: '<img class="arrow arrow_left" src="images/arrow left.svg" alt="arrow">',
+    nextArrow: '<img class="arrow arrow_right" src="images/arrow roght.svg" alt="arrow">',
   });
 
 });
