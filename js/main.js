468ab0a2409435fadb2f7a8f78c0616177a1a4da
Den T <tsyhanenko.dp@gmail.com>
2022-09-06 09:39:24 +0300

    adapt slider

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ $(function(){
     variableWidth: true,
     prevArrow: '<img class="arrow arrow_left" src="images/arrow left.svg" alt="arrow">',
     nextArrow: '<img class="arrow arrow_right" src="images/arrow roght.svg" alt="arrow">',
+    responsive:[
+      {
+        breakpoint: 711,
+        settings:{
+          variableWidth: false,
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 
 });
